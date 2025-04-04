14c5665bd72013379efbaa109e18a530013d0b86
edwinaguila <edwinaguila00@gmail.com>
2025-04-04 16:09:43 +0000

    To my knowledge, querySelector() allows elements to be selected using CSS selectors such as # for id or . for class. In this example, only used classes

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,15 @@ const title = document.getElementById("title");
 
 console.log(title);
 
+let otherTitle = document.querySelector(".otherTitle");
+
+console.log(otherTitle);
+
+let bio = document.querySelector(".bio");
+
+console.log(bio);
+
+let noTouch = document.querySelector(".noTouch");
+
+console.log(noTouch);
 
